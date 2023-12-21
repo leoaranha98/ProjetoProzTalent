79159fc03ab15ec825c3d766aa4bc0683b904048
JeanCarlosAlmeida <jean.carlos.almeida1989@gmail.com>
2023-12-21 16:45:51 -0300

    Atualização_4 | Página_Cadastro

---
+++ cadastro/cadastro.js
@@ -0,0 +1,11 @@
+let texto = document.querySelector(".sec4 p")
+console.log(texto)
+texto.style.fontSize = "10px"
+
+let button = document.querySelector(".sec5 button")
+console.log(button)
+// button.style.marginBotton = "20px"
+
+let label = document.querySelectorAll(".sec1 label")
+console.log(label)
+// label.style.fontSize = "15px"
